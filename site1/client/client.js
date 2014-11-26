29121c48175eed0425291d35e6e497b4363a60b1
Rushi Shah <2016rshah@gmail.com>
2014-11-25 21:33:38 -0500

    Lots of progress once again. More info, cool animations, etc.

--- site1/client/client.js
+++ site1/client/client.js
@@ -14,11 +14,41 @@ if (Meteor.isClient) {
             Session.set("counter", Session.get("counter") + 1);
         }
     });
-    Template.blockquote.rendered = function () {
-        animate("blockquote", "swing")
+    // Template.blockquote.rendered = function () {
+    //     animate("blockquote", "bounce")
+    // };
+    // Template.blockquote.events({
+    //     'click blockquote': function () {
+    //         animate("blockquote", "hinge")
+    //     }
+    // });
+    Template.projects.rendered = function () {
+        animate("#hacks", "flipInX")
+        animate("#classes", "flipInX")
+    };
+    // Template.projects.events({
+    //     'click #hacks': function () {
+    //         animate("#hacks", "flipInX")
+    //     }, 
+    //     'click #classes':function(){
+    //         animate("#classes", "flipInX")
+    //     }
+    // });
+    Template.navigation.rendered = function () {
+        $(function(){
+            $(".element").typed({
+                strings: ["System.out.print('Hello World!');", "print('Hello World!')", "console.log('Hello World!')", "Hello World!"],
+                typeSpeed: .25
+            });
+        });
+    };
+    Template.references.rendered = function () {
+        animate("#references", "fadeInUp")
     };
     function animate(selector, anim) {
         $(selector).removeClass().addClass(anim + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            if(anim == "hinge")
+                $(this).hide()
             $(this).removeClass();
         });
     };
